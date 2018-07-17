6e784317236d53c5ce5ca075cbf9ff590672d176
Kira Hartness <kirahartness@gmail.com>
2018-07-17 13:36:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "It was so awesome to see Karlie yesterday! She put alot of out pictures and videos on her insta!"
+    "It was so awesome to see Karlie yesterday! She put alot of out pictures and videos on her insta! I love her so much! "
   end
 
 end
